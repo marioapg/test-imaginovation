fc06fa641dfc684da358b9f7c43b0fec41566ade
MarioDev <mario.perez@technoglobalinc.com>
2025-04-02 13:50:24 -0400

    ADD script to find timezones and users and send email notification

---
+++ app/Console/Commands/SendDailyEmails.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Notifications\DailyReportNotification;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use App\Models\Timezone;
+use App\Models\User;
+use Carbon\Carbon;
+
+class SendDailyEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-daily-emails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send daily emails to users at 5 PM in their local timezone';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $utcNow = Carbon::now('UTC');
+        // $this->info('Current UTC time: ' . $utcNow->toDateTimeString());
+        $utcHour = $utcNow->hour;
+        $utcMinute = $utcNow->minute;
+
+        $matchingTimezones = Timezone::whereRaw("
+            ((? + offset_hours + FLOOR(offset_minutes / 60)) % 24) = 13
+            AND ((? + offset_minutes) % 60) = 45
+        ", [$utcHour, $utcMinute])->get();
+
+        if ($matchingTimezones->isEmpty()) {
+            $this->info('No matching timezones found for 5:00 PM.');
+            return;
+        }
+
+        $this->info('Timezones where it is currently 5:00 PM:');
+        foreach ($matchingTimezones as $tz) {
+            $this->line(" - {$tz->name} (UTC {$tz->offset_hours}:{$tz->offset_minutes})");
+        }
+        $tzIds = $matchingTimezones->pluck('id')->toArray();
+        $users = User::whereIn('timezone_id', $tzIds)->get();
+
+        foreach ($users as $user) {
+            $this->info('Sending email to: ' . $user->email);
+            $code = Str::random(10);
+            $user->notify(new DailyReportNotification($code));
+        }
+    }
+}
---
+++ app/Notifications/DailyReportNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DailyReportNotification extends Notification
+{
+    use Queueable;
+
+    private $report;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($report)
+    {
+        $this->report = $report;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Daily Report')
+            ->line('This is your daily mail report with the code: ' . $this->report);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
